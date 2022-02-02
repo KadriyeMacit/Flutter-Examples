09ea87b2504d218105f1624b74db4ee5cdc5a05c
Kadriye <kadriyemacit9655@gmail.com>
2022-02-02 20:19:50 +0000

    Webview örneği eklendi.

---
+++ flutter_webview/android/app/src/main/kotlin/com/example/flutter_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_webview/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: SafeArea(
+      child: WebView(
+        initialUrl: 'https://turkish.chat',
+      ),
+    ));
+  }
+}
